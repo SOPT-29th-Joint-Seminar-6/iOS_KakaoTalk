32ba1a6efc49c9acd58a29d8e3e449b0aa3dc6f4
asdasd <a9327370@gmail.com>
2021-11-25 23:03:27 +0900

    [#7] color extension 추가

--- iOS_KakaoTalk/iOS_KakaoTalk/Configuration/Extension/UIColor.swift
+++ iOS_KakaoTalk/iOS_KakaoTalk/Configuration/Extension/UIColor.swift
@@ -5,4 +5,50 @@
 //  Created by Yi Joon Choi on 2021/11/13.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, a: Int = 0xFF) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: CGFloat(a) / 255.0
+        )
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
+internal extension UIColor {
+    @nonobjc class var darkGray: UIColor {
+        return UIColor(rgb: 0x191919)
+    }
+    
+    @nonobjc class var gray4: UIColor {
+        return UIColor(rgb: 0x747474)
+    }
+    
+    @nonobjc class var gray3: UIColor {
+        return UIColor(rgb: 0xA3A3A3)
+    }
+    
+    @nonobjc class var gray2: UIColor {
+        return UIColor(rgb: 0xB9B9B9)
+    }
+    
+    @nonobjc class var gray1: UIColor {
+        return UIColor(rgb: 0xC4C4C4)
+    }
+    
+    @nonobjc class var pointColor: UIColor {
+        return UIColor(rgb: 0xEA6039)
+    }
+}
+
