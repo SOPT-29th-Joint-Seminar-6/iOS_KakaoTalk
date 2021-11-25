1cd3d26dc059b281ebfe19d0c58e227b82acfee7
최이준 <choi990406@gmail.com>
2021-11-26 04:57:46 +0900

    [#9] 발견 뷰 tableView cell UI

---
+++ iOS_KakaoTalk/iOS_KakaoTalk/Sources/Cell/DiscoveryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DiscoveryTableViewCell.swift
+//  iOS_KakaoTalk
+//
+//  Created by Yi Joon Choi on 2021/11/26.
+//
+
+import UIKit
+
+class DiscoveryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
