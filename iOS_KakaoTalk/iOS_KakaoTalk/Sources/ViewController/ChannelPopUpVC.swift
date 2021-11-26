a5627354b41961f70ffecf9ff19c40f5de537666
dlwns33 <choi990406@gmail.com>
2021-11-26 18:24:40 +0900

    [#11] 팝업 dismiss

--- iOS_KakaoTalk/iOS_KakaoTalk/Sources/ViewController/ChannelPopUpVC.swift
+++ iOS_KakaoTalk/iOS_KakaoTalk/Sources/ViewController/ChannelPopUpVC.swift
@@ -10,6 +10,19 @@ import UIKit
 class ChannelPopUpVC: UIViewController {
 
     @IBOutlet var background: UIView!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var channelNameLabel: UILabel!
+    @IBOutlet weak var friendLabel: UILabel!
+    @IBOutlet weak var channelDescLabel: UILabel!
+    
+    @IBOutlet weak var explainLabel: UILabel!
+    
+    @IBAction func tapToCancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func tapToAddChannel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +31,7 @@ class ChannelPopUpVC: UIViewController {
     
     private func setUI(){
         background.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        explainLabel.text = ""
     }
 
 }
