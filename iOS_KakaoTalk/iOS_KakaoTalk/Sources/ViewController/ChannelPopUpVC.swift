1b2f12081057f9047715cc0fc8d121a69d6f9a9d
dlwns33 <choi990406@gmail.com>
2021-11-26 18:56:41 +0900

    [#11] 팝업 더미 데이터

--- iOS_KakaoTalk/iOS_KakaoTalk/Sources/ViewController/ChannelPopUpVC.swift
+++ iOS_KakaoTalk/iOS_KakaoTalk/Sources/ViewController/ChannelPopUpVC.swift
@@ -10,11 +10,13 @@ import UIKit
 class ChannelPopUpVC: UIViewController {
 
     @IBOutlet var background: UIView!
+    @IBOutlet weak var popUpView: UIView!
     @IBOutlet weak var icon: UIImageView!
     @IBOutlet weak var channelNameLabel: UILabel!
     @IBOutlet weak var friendLabel: UILabel!
     @IBOutlet weak var channelDescLabel: UILabel!
     
+    @IBOutlet weak var explainTitleLabel: UILabel!
     @IBOutlet weak var explainLabel: UILabel!
     
     @IBAction func tapToCancel(_ sender: Any) {
@@ -31,7 +33,23 @@ class ChannelPopUpVC: UIViewController {
     
     private func setUI(){
         background.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        explainLabel.text = ""
+        popUpView.layer.cornerRadius = 8
+        
+        let grayLabels = [friendLabel, explainTitleLabel, explainLabel]
+        
+        grayLabels.forEach{
+            $0!.textColor = .gray3
+        }
+        
+        explainLabel.text = "광고와 마케팅 메세지를 카카오톡으로 받아보실 수 있습니다.\n채널의 보드를 My뷰에서 모아볼 수 있습니다."
+        explainLabel.lineBreakMode = .byWordWrapping
+        explainLabel.numberOfLines = 3
+        explainLabel.textAlignment = .left
+        
+        channelDescLabel.textColor = .systemGray
+        channelDescLabel.lineBreakMode = .byWordWrapping
+        channelDescLabel.numberOfLines = 2
+        channelDescLabel.textAlignment = .left
     }
 
 }
