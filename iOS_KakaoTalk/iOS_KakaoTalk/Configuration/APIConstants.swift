10e173de7ddee62b12afa9b581a8b31164e8f4e8
asdasd <a9327370@gmail.com>
2021-12-03 16:28:39 +0900

    [#17] API Constants 추가

---
+++ iOS_KakaoTalk/iOS_KakaoTalk/Configuration/APIConstants.swift
@@ -0,0 +1,17 @@
+//
+//  APIConstants.swift
+//  iOS_KakaoTalk
+//
+//  Created by 김인환 on 2021/12/03.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL: String = "https://asia-northeast3-server-again.cloudfunctions.net/api"
+    
+    static let getArticle: String = baseURL + "/view/viewfind"
+    static let getKakaoTVThumbnail: String = baseURL + "/view/viewthumb"
+    static let addPlusChannel: String = baseURL + "/channel"
+    static let getPlusChannelInfo: (String) -> String = { id in baseURL + "/channel/" + id }
+}
